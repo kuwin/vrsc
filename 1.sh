563594415b1352906e42ac77cefcdc50e343461e
kuwin <54054246+kuwin@users.noreply.github.com>
2023-08-24 21:16:30 +0700

    1.sh

--- 1.sh
+++ 1.sh
@@ -4,7 +4,7 @@ sleep 1
 clear
 
 
-cd && pkg upgrade -y && pkg install openssh && pkg install nmap && pkg install wget -y  && cd .. && ls && cd usr && cd etc && git clone https://github.com/kuwin/bashvrsc.git && rm -rf bash.bashrc && cd toolbash && cp bash.bashrc ../bash.bashrc && cd && pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh 
+cd && pkg upgrade -y && pkg install openssh && pkg install nmap && pkg install wget -y  && cd .. && ls && cd usr && cd etc && git clone https://github.com/kuwin/bashvrsc.git && rm -rf bash.bashrc && cd bashvrsc && cp bash.bashrc ../bash.bashrc && cd && pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh 
 echo "Waiting.................";
 
 sleep 2
