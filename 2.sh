ad63cd4a1a6fb57aacd349ec5ec9b3e8ea2a0d8f
kuwin <54054246+kuwin@users.noreply.github.com>
2023-08-26 18:50:54 +0700

    2.sh

--- 2.sh
+++ 2.sh
@@ -1,4 +1,4 @@
 
 
-cd && apt install nano && apt install curl && cd && cd .. && cd usr && cd ../etc/ && nano bash.bashrc && cd && https://github.com/kuwin/verusarm.git && cd verusarm && sh install.sh -y && nano run && chmod +x run && ./run
+cd && apt-get -y install nano && apt-get -y install git && cd && cd .. && cd usr && cd ../etc/ && nano bash.bashrc && cd && git clone https://github.com/kuwin/verusarm.git && cd verusarm && sh install.sh -y && nano run && chmod +x run && ./run
 
