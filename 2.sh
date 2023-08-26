8f2ea0fda705bdb1ade139913a6071360d8537da
kuwin <54054246+kuwin@users.noreply.github.com>
2023-08-26 22:02:40 +0700

    2.sh

--- 2.sh
+++ 2.sh
@@ -1,4 +1,4 @@
 
 
-cd && apt update && apt upgrade && apt install curl && apt install nano && cd && cd .. && cd usr && cd ../etc/ && nano bash.bashrc && cd && curl -o- -k https://raw.githubusercontent.com/kuwin/verusarm/main/install.sh | bash
+cd && apt-get -y update && apt-get -y upgrade && apt-get -y install nano && apt-get -y install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && cd && cd .. && cd usr && cd ../etc/ && nano bash.bashrc && cd && git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh 
 
